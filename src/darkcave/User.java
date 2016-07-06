eadd88699bea1220ceda70aa3ba3d62e0d2e67c1
Tyler Hensley <Tyler Hensley>
2016-07-06 16:23:48 -0400

    Initial Commit

---
+++ src/darkcave/User.java
@@ -0,0 +1,45 @@
+package darkcave;
+
+public class User {
+	private int id;
+	private String name;
+	private String password;
+	private String email;
+	private String fullname;
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	
+	
+	
+	
+	
+}
